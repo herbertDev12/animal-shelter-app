793407b7f63eebbba2c022e2b1163fa04c246e99
herbertDev12 <herbertn740@gmail.com>
2026-05-30 23:31:20 -0500

    chore: set up commitlint and husky for commit message validation and pre-commit hooks

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -15,5 +15,14 @@ module.exports = {
   },
   rules: {
     "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        "argsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_"
+      }
+    ],
+    "no-unused-vars": "off"
   },
 };
